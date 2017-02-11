147ca09e21072a542a1c19bf901ccc162c2ca18e
GalaxyDeveloper <davidjavaone@gmail.com>
2017-02-11 10:30:24 -0600

    wowie

--- src/com/gateam/plugin/Plugin1Listener.java
+++ src/com/gateam/plugin/Plugin1Listener.java
@@ -16,25 +16,45 @@ import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.entity.EntityTargetEvent.TargetReason;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class Plugin1Listener implements Listener {
 	// Dialog
+	private static final String MYSTERY1 = "\"You wake up, your memories fade a little...\"";
+	private static final String MYSTERY2 = "\"You feel lightheaded, your memories fade a little...\"";
+	private static final String MYSTERY3 = "\"You feel as if you were wounded, you remember less then before...\"";
 	private static final String SENCOUNTER1 = "I will capture a human!";
 	private static final String SENCOUNTER2 = "You've asked for it.";
+	private static final String SENCOUNTER3 = "Stop, human!";
 	private static final String SKILL_MESSAGE = "Spare me, please!";
 	private static final String SFOLLOWING1 = "Then, I will receive all the things I utterly deserve!";
+	private static final String SFOLLOWING2 = "I was sent to kill you!";
 	private static final String ZENCOUNTER1 = "Brains!";
 	private static final String ZENCOUNTER2 = "Brains...";
 	private static final String ZKILL_MESSAGE = "Brains?";
 	private static final String ZFOLLOWING1 = "Brraaiinnnss!";
+	private static final String SPENCOUNTER1 = "I've heard of a human coming through, Sssss...";
+	private static final String SPENCOUNTER2 = "You seem suspicious, Sssss...";
+	private static final String SPKILL_MESSAGE = "You slippery human! Sssss...";
+	private static final String SPFOLLOWING1 = "Sssss...";
 
-	// Player Join Message
+	// Player Death Quotes
 	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent event) {
-		Bukkit.broadcastMessage("This Server Uses Minequest v.1.0");
+	public void playerDeathQuotes(PlayerDeathEvent event) {
+		Random rand = new Random();
+		int r = rand.nextInt(100);
+		String message = null;
+		if (r <= 10) {
+			Bukkit.broadcastMessage(message = MYSTERY1);
+		} else if (r >= 85) {
+			Bukkit.broadcastMessage(message = MYSTERY2);
+		} else if (r >= 43 && r <= 56) {
+			Bukkit.broadcastMessage(message = MYSTERY3);
+		} else {
+			message = null;
+		}
+
 	}
 
 	/////////////////////////
@@ -49,10 +69,12 @@ public class Plugin1Listener implements Listener {
 			if (entity.getType() == EntityType.SKELETON) {
 				Bukkit.broadcastMessage(ChatColor.RED + "You feel like your going to have a bad time...");
 				Random rand = new Random();
-				int r = rand.nextInt(2);
+				int r = rand.nextInt(3);
 				String message = null;
 				if (r == 0) {
 					message = SENCOUNTER1;
+				} else if (r == 2) {
+					message = SENCOUNTER3;
 				} else {
 					message = SENCOUNTER2;
 				}
@@ -65,6 +87,8 @@ public class Plugin1Listener implements Listener {
 					public void run() {
 						if (SENCOUNTER1.equals(entity.getCustomName())) {
 							entity.setCustomName(SFOLLOWING1);
+						} else if (SENCOUNTER3.equals(entity.getCustomName())) {
+							entity.setCustomName(SFOLLOWING2);
 						} else {
 							entity.setCustomName(null);
 							entity.setCustomNameVisible(false);
@@ -95,7 +119,7 @@ public class Plugin1Listener implements Listener {
 		}
 	}
 
-	// Skeleton Death Message Bug Fixed
+	// All Death Messages Fixed
 	@EventHandler
 	public void onKill(PlayerDeathEvent e) {
 		String killed = e.getEntity().getName();
@@ -155,6 +179,55 @@ public class Plugin1Listener implements Listener {
 		final Entity e = event.getEntity();
 		if (e.getType() == EntityType.ZOMBIE) {
 			e.setCustomName(ZKILL_MESSAGE);
+			e.setCustomNameVisible(true);
+		}
+	}
+
+	///////////////////////
+	// Spider - RPG Edits//
+	///////////////////////
+
+	// Spider Quotes
+	@EventHandler
+	public void spiderEncounterQuotes(EntityTargetLivingEntityEvent event) {
+		if (event.getReason() == TargetReason.CLOSEST_PLAYER) {
+			Entity entity = event.getEntity();
+			if (entity.getType() == EntityType.SPIDER) {
+				Bukkit.broadcastMessage(ChatColor.RED + "You hear something hissing, it's getting louder...");
+				Random rand = new Random();
+				int r = rand.nextInt(2);
+				String message = null;
+				if (r == 0) {
+					message = SPENCOUNTER1;
+				} else {
+					message = SPENCOUNTER2;
+				}
+				entity.setCustomName(message);
+				entity.setCustomNameVisible(true);
+				Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin(Plugin1.PLUGIN_NAME);
+				BukkitRunnable bukkitRunnable1 = new BukkitRunnable() {
+
+					@Override
+					public void run() {
+						if (SPENCOUNTER1.equals(entity.getCustomName())) {
+							entity.setCustomName(SPFOLLOWING1);
+						} else {
+							entity.setCustomName(null);
+							entity.setCustomNameVisible(false);
+						}
+					}
+				};
+				bukkitRunnable1.runTaskLater(plugin, 140L);
+			}
+		}
+	}
+
+	// Spider Last Words
+	@EventHandler
+	public void onMobKilledSpider(EntityDeathEvent event) {
+		final Entity e = event.getEntity();
+		if (e.getType() == EntityType.SPIDER) {
+			e.setCustomName(SPKILL_MESSAGE);
 			e.setCustomNameVisible(true);
 		}
 	}
